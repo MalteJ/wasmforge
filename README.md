8d33c4c8dcd79f48523ed6766e834ff5f3d04d8a
Malte Janduda <mail@janduda.net>
2024-11-29 17:56:10 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+WasmForge
+=========
+
+### License
+
+FeBGP is licensed under [Apache License v2.0](LICENSE).
---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use log::{debug, info, LevelFilter};
+use simple_logger::SimpleLogger;
+use wasmer::{Instance, Module, Store, imports, Value};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    SimpleLogger::new().env().with_level(LevelFilter::Debug).init().unwrap();
+    info!("WasmForge is starting...");
+
+    debug!("Reading wasm code...");
+    let wasm_bytes = std::fs::read("./example/helloworld/build/debug.wasm")?;
+
+    debug!("creating store...");
+    let mut store = Store::default();
+
+    debug!("creating module...");
+    let module = Module::new(&store, &wasm_bytes)?;
+
+    debug!("defining imports...");
+    let import_object = imports! {};
+
+    debug!("creating instance...");
+    let instance = Instance::new(&mut store, &module, &import_object)?;
+
+    debug!("getting exports....");
+    let main_function = instance.exports.get_function("add_one")?;
+
+    info!("Executing main...");
+    let result = main_function.call(&mut store, &[Value::I32(41)])?;
+
+    info!("The answer is {}", result[0]);
+
+    Ok(())
+}
